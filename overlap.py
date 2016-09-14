13bf3492c8557d2f9e9864c2e9cb50d947879733
Eugenio Mattei <eugenio.mattei@umassmed.edu>
2016-09-14 10:44:34 -0400

    - First Commit - backbone of the structure

---
+++ overlap.py
@@ -0,0 +1,22 @@
+########################################################################
+# psychip - ChIP-seq pipeline for psychENCODE project
+# Copyright (C) 2016  Weng's Lab @ UMASS medical school - Worcester (MA)
+#
+# Developers:
+# Eugenio Mattei : eugenio.mattei@umassmed.edu
+# Junko Tsuji : junko.tsuji@umassmed.edu
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+########################################################################
+
